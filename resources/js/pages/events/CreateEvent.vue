7fa22b07dc552bab6c2521e9da7db6ab797f2901
seklyza <seklyza@users.noreply.github.com>
2020-10-20 15:26:04 +0300

    moved the CreateEvent component to TS and cAPI

--- resources/js/pages/events/CreateEvent.vue
+++ resources/js/pages/events/CreateEvent.vue
@@ -5,39 +5,39 @@
         <base-form-input
           name="name"
           label="Event Name"
-          v-model.trim="name"
+          v-model.trim="form.name"
         ></base-form-input>
         <base-form-textarea
           name="description"
           label="Event Description"
           rows="10"
-          v-model.trim="description"
+          v-model.trim="form.description"
         ></base-form-textarea>
         <base-form-input
           type="date"
           name="datetime"
           label="Event Date"
-          v-model.trim="date"
+          v-model.trim="form.date"
           max="2028-12-31"
         ></base-form-input>
         <base-form-input
           type="time"
           name="time"
           label="Event Time"
-          v-model.trim="time"
+          v-model.trim="form.time"
           :show-errors="false"
         ></base-form-input>
         <base-form-input
           type="location"
           name="location"
           label="Event Location"
-          :model-value="location"
+          :model-value="form.location"
           @input="onLocationUpdate"
         ></base-form-input>
         <simple-map
-          v-if="location"
+          v-if="form.location"
           class="mt-4"
-          :location="location"
+          :location="form.location"
         ></simple-map>
         <base-button class="p-3 rounded-md">Create Event</base-button>
       </form>
@@ -45,51 +45,51 @@
   </main-layout>
 </template>
 
-<script>
+<script lang="ts" setup>
 import _debounce from 'lodash/debounce'
 
 import SimpleMap from '../../components/maps/SimpleMap.vue'
 import { Inertia } from '@inertiajs/inertia'
+import { reactive } from 'vue'
+
+export const form = reactive({
+  name: '',
+  description: '',
+  date: '',
+  time: '',
+  location: '',
+})
+
+export function onSubmit() {
+  const datetime = (() => {
+    try {
+      const datetime = new Date(form.date)
+      const [hours, minutes] = form.time.split(':')
+      datetime.setHours(hours as any)
+      datetime.setMinutes(minutes as any)
+      return datetime
+    } catch (e) {
+      return ''
+    }
+  })()
+
+  const formData = {
+    name: form.name,
+    description: form.description,
+    datetime,
+    location: form.location,
+  }
+
+  Inertia.post(route('events.store'), formData)
+}
+
+export const onLocationUpdate = _debounce(e => {
+  form.location = e.target.value
+}, 1000)
 
 export default {
   components: {
     SimpleMap,
-  },
-  data() {
-    return {
-      name: '',
-      description: '',
-      date: '',
-      time: '',
-      location: '',
-    }
-  },
-  methods: {
-    onSubmit() {
-      const datetime = (() => {
-        try {
-          const datetime = new Date(this.date)
-          const [hours, minutes] = this.time.split(':')
-          datetime.setHours(hours)
-          datetime.setMinutes(minutes)
-          return datetime
-        } catch (e) {
-          return ''
-        }
-      })()
-
-      const formData = {
-        name: this.name,
-        description: this.description,
-        datetime,
-        location: this.location,
-      }
-
-      Inertia.post(route('events.store'), formData)
-    },
-    onLocationUpdate: _debounce(function(e) {
-      this.location = e.target.value
-    }, 1000),
   },
 }
 </script>
