ccfa6f5192e0bcf120e320f4522891c4c302e42b
seklyza <seklyza@users.noreply.github.com>
2020-10-20 23:10:09 +0300

    renamed the events test class name

--- tests/Feature/EventsTest.php
+++ tests/Feature/EventsTest.php
@@ -8,7 +8,7 @@ use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class EventsControllerTest extends TestCase
+class EventsTest extends TestCase
 {
     use RefreshDatabase;
 
